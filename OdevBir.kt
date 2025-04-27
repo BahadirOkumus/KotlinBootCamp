f2ab1c1427b58060703a58f45baca84749ae43e1
BahadirOkumus <93206066+BahadirOkumus@users.noreply.github.com>
2025-04-27 16:10:37 +0300

    First assignment completed

---
+++ OdevBir.kt
@@ -0,0 +1,55 @@
+package com.example.odevkotlin.Odev1
+
+fun main(){
+    val sehir="Istanbul"
+    val ulke="Turkiye"
+    val telefon= 12224445
+    val postakodu=2000
+    val email= "oku.baha@gmail.com"
+    val meslek="ogretmen"
+    val stokMiktari= 20
+    val musteriAdi="halil"
+    val bakiye= 0
+    val dogumGunu="20.02.2000"
+    val maas=2000
+    val medeniDurum= "Bekar"
+    val ödeme=20000
+    val marka="Nike"
+    var muzikAdı="Rihanna"
+    var renk="Mavi"
+    var kalanDakika=24.5
+    var odaSayısı=2
+    var enlem= 24.52
+    var boylam=12.42
+    var agirlik=120
+
+    println("sehir : $sehir")
+    println("ulke : $ulke")
+    println("telefon : $telefon")
+    println("postakodu : $postakodu")
+    println("email : $email")
+    println("meslek : $meslek")
+    println("stokMiktari : $stokMiktari")
+    println("musteriAdi : $musteriAdi")
+    println("bakiye : $bakiye")
+    println("dogumGunu : $dogumGunu")
+    println("maas : $maas")
+    println("medeniDurum : $medeniDurum")
+    println("ödeme : $ödeme")
+    println("marka : $marka")
+    println("muzikAdı : $muzikAdı")
+    println("renk : $renk")
+    println("kalanDakika : $kalanDakika")
+    println("odaSayısı: $odaSayısı")
+    println("enlem : $enlem")
+    println("boylam : $boylam")
+    println("agirlik : $agirlik")
+
+
+
+
+
+
+
+
+}
